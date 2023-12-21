518eaffd3960d7157d4e8e8bca90d4e19a57eae4
kuco23 <nseverkar@gmail.com>
2023-12-22 00:26:09 +0100

    small frontend fixes

--- contracts/KucoCoin.sol
+++ contracts/KucoCoin.sol
@@ -5,33 +5,38 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "blazeswap/contracts/periphery/interfaces/IBlazeSwapRouter.sol";
 
+struct MenstruationEntry {
+    mapping(uint16 => uint32) entry;
+    uint16 index;
+}
+
 contract KucoCoin is ERC20, Ownable {
 
     // constant vars :)
-    address immutable public feePool = address(0xdeadaf);
+    address immutable public burnAddress = 0x7B1aFE2745533D852d6fD5A677F14c074210d896;
+    address immutable public wNat;
+    IBlazeSwapRouter immutable public blazeSwapRouter;
 
-    // constants set on deploy
-    address public wNat;
-    IBlazeSwapRouter public blazeSwapRouter;
-
-    // if token tranasctions are disabled
+    // disables all token transactions
     bool public disabled = false;
 
-    // menstruation tracking
-    struct MenstruationEntry {
-        mapping(uint16 => uint32) entry;
-        uint16 index;
-    }
+    // tracking
+    mapping(address => uint256) public invested;
     mapping(address => MenstruationEntry) private menstruation;
 
-    constructor(address _wNat, IBlazeSwapRouter _blazeSwapRouter) ERC20("KucoCoin", "KUCO") {
+    constructor(
+        address _wNat,
+        IBlazeSwapRouter _blazeSwapRouter
+    )
+        ERC20("KucoCoin", "KUCO")
+    {
         wNat = _wNat;
         blazeSwapRouter = _blazeSwapRouter;
         _mint(msg.sender, 100 ether);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    // blazeswap integration
+    // blazeswap integration for buying and investing
 
     modifier dexApprove(uint256 _amount) {
         _transfer(msg.sender, address(this), _amount);
@@ -40,13 +45,24 @@ contract KucoCoin is ERC20, Ownable {
         _approve(address(this), address(blazeSwapRouter), 0);
     }
 
-    function addLiquidity(uint256 _amount) external payable dexApprove(_amount) {
+    function addLiquidity(
+        uint256 _amount,
+        address _receiver
+    )
+        external payable
+        dexApprove(_amount)
+    {
         blazeSwapRouter.addLiquidityNAT{value: msg.value}(
-            address(this), _amount, 0, 0, 0, msg.sender, block.timestamp
+            address(this), _amount, 0, 0, 0, _receiver, block.timestamp
         );
     }
 
-    function buy(address _receiver, uint256 _minKuco) external payable {
+    function buy(
+        address _receiver,
+        uint256 _minKuco
+    )
+        external payable
+    {
         address[] memory path = new address[](2);
         path[0] = wNat;
         path[1] = address(this);
@@ -55,17 +71,38 @@ contract KucoCoin is ERC20, Ownable {
         );
     }
 
-    function sell(address _receiver, uint256 _amount, uint256 _minNat) external dexApprove(_amount) {
+    function sell(
+        address _receiver,
+        uint256 _amount,
+        uint256 _minNat
+    )
+        external
+        dexApprove(_amount)
+    {
         address[] memory path = new address[](2);
         path[0] = address(this);
         path[1] = wNat;
-        blazeSwapRouter.swapExactTokensForNAT(
-            _amount, _minNat, path, _receiver, block.timestamp
-        );
+        blazeSwapRouter.swapExactTokensForNAT(_amount, _minNat, path, _receiver, block.timestamp);
     }
 
-    function getPoolReserves() external view returns (uint256, uint256) {
+    function getPoolReserves()
+        external view
+        returns (uint256, uint256)
+    {
         return blazeSwapRouter.getReserves(address(this), address(wNat));
+    }
+
+    // invest in kucocoin to claim the distribution
+    function invest(
+        address _receiver,
+        uint256 _amount
+    )
+        external payable
+    {
+        blazeSwapRouter.addLiquidityNAT{value: msg.value}(
+            address(this), _amount, 0, 0, 0, address(0), block.timestamp
+        );
+        invested[_receiver] += _amount;
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -73,22 +110,32 @@ contract KucoCoin is ERC20, Ownable {
 
     // function disable()
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    )
+        internal override
+    {
         require(!disabled, "KucoCoin: token transfers are disabled");
         super._beforeTokenTransfer(from, to, amount);
     }
 
-    // kucocoin specific functionality
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // methods that make kucocoin the best token
 
-    function description() external pure returns (string memory) {
-        return "KucoCoin is the best token in the world!";
-    }
-
-    function reportMenstruation() external {
+    function reportMenstruation()
+        external
+    {
         menstruation[msg.sender].entry[menstruation[msg.sender].index++] = uint32(block.timestamp);
     }
 
-    function makeTransAction(address _to, uint256 _amount) external {
+    function makeTransAction(
+        address _to,
+        uint256 _amount
+    )
+        external
+    {
         _transfer(msg.sender, _to, _amount);
     }
 }
